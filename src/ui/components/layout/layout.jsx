02f83db040222ca1805a172c5504a5c567c265a2
hassanmal69 <158709739+hassanmal69@users.noreply.github.com>
2026-05-31 01:14:31 +0500

    book/ui/changed

--- src/ui/components/layout/layout.jsx
+++ src/ui/components/layout/layout.jsx
@@ -1,13 +1,17 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../navbar/navbar.jsx";
 import Sidebar from "../sidebar/sidebar.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
   const navigate = useNavigate();
   const location = useLocation();
   const activePage = location.pathname.replace("/", "") || "dashboard";
-
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    }
+  }, [navigate,activePage]);
   const [user, setUser] = useState({
     name: "Julian Thomas",
     id: "LBN#A4512",
@@ -20,7 +24,7 @@ export default function Layout() {
       <Sidebar activePage={activePage} onNavigate={(id) => navigate(id === "dashboard" ? "/" : `/${id}`)} />
       <Navbar user={user} onUpdateUser={setUser} />
       <main style={{ marginLeft: 220, paddingTop: 60 }}>
-        <Outlet /> 
+        <Outlet />
       </main>
     </div>
   );
